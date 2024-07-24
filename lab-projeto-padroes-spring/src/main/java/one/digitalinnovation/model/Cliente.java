14d14d9d942fea6b8d461b13cf4580495058f358
mariaato <mariaaatonha@gmail.com>
2024-07-23 22:12:48 -0300

    projeto

---
+++ lab-projeto-padroes-spring/src/main/java/one/digitalinnovation/model/Cliente.java
@@ -0,0 +1,38 @@
+package one.digitalinnovation.model;
+
+
+@jakarta.persistence.Entity
+public class Cliente {
+
+	@org.springframework.data.annotation.Id
+	@jakarta.persistence.GeneratedValue(strategy = jakarta.persistence.GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	@jakarta.persistence.ManyToOne
+	private Endereco endereco;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+}
